1206c76b0979e2ff179b887d06ab291824b650bd
Azrael <dkolya2527@gmail.com>
2026-05-31 00:20:31 +0300

    Создал осовную архитектуру класса

--- src/classifier.py
+++ src/classifier.py
@@ -1,3 +1,25 @@
 class Classifier:
-    def __init__(self, emails):
+    # Принимаю в качестве аргумента все письма(нужно создать под них класс) и файл правил
+    def __init__(self, emails, rules_path):
+        self.emails = emails
+        self.rules_to_categorize = self.get_rules_to_categorize(rules_path)
+
+    # Классифицирует все предоставленные письма, сохраняет в поле класса
+    def classify_all(self):
+        pass
+
+    # Классифицирует конкретное письмо, вспомогательный
+    def classify(self, email):
+        pass
+
+    # Возвращает результат в формате: Путь -> Категория
+    def get_email_to_category(self):
+        pass
+
+    # Возвращает результат в формате: Категория -> Количество писем, также общее количество писем
+    def get_analytics(self):
+        pass
+
+    # Получает правила для распределения писем на категории (думаю сделать через json файл)
+    def get_rules_to_categorize(self, path):
         pass
