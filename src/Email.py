ad35c952fc7e3ca7203fa0b1a795f0959b044b24
Azrael <dkolya2527@gmail.com>
2026-05-31 02:23:36 +0300

    создал класс для писем

---
+++ src/Email.py
@@ -0,0 +1,9 @@
+class Email:
+    def __init__(self,file_name, path, sender, receiver, date, subject, text):
+        self.file_name = file_name
+        self.path = path
+        self.sender = sender
+        self.receiver = receiver
+        self.date = date
+        self.subject = subject
+        self.text = text
