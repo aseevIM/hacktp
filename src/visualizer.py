9ec63f0eca7ee5e06766c436c0749423dccaa6c7
Azrael <dkolya2527@gmail.com>
2026-05-31 12:36:03 +0300

    основная архитектура визуализатора

---
+++ src/visualizer.py
@@ -0,0 +1,18 @@
+class Visualizer:
+    def __init__(self):
+        pass
+
+    def get_analytics(self, classified_data):
+        pass
+
+    def get_schedule(self):
+        pass
+
+    def build_bar(self):
+        pass
+
+    def build_pie(self):
+        pass
+
+
+
