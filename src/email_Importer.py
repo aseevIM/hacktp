800c8eaf35c1f88fee5d1f4a4ab68629f18ce68f
Azrael <dkolya2527@gmail.com>
2026-05-31 23:38:44 +0300

    Основная архитектура класса EmailImporter

---
+++ src/email_Importer.py
@@ -0,0 +1,12 @@
+class EmailImporter:
+    def __init__(self, input_dir):
+        pass
+
+    # Читает все письма, возвращает список экземпляров Email
+    def read_all_emails(self):
+        pass
+
+    #Прочитать одно письмо, представить в виде экземпляра класса Email, вспомогательный
+    def read_email(self):
+        pass
+
