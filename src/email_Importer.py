8112a79bacd24a82d3585b277c3af84136ad6330
Azrael <dkolya2527@gmail.com>
2026-06-01 02:18:32 +0300

    Создал класс Email в email_class.py, реализовал метод read_email класса EmailImporter для чтения конкретного письма в email_Importer.py

--- src/email_Importer.py
+++ src/email_Importer.py
@@ -1,12 +1,42 @@
+from email_class import Email
+
+
 class EmailImporter:
     def __init__(self, input_dir):
-        pass
+        # возможные варианты записи конкретных полей, беру только поле subject, остальные не несут никакой
+        # важной для классификации информации
+        # !!!! Обсудить
+        self.subject_v = {"тема", "subject", "tema"}
 
     # Читает все письма, возвращает список экземпляров Email
     def read_all_emails(self):
         pass
 
-    # Прочитать одно письмо, представить в виде экземпляра класса Email, вспомогательный
-    def read_email(self):
-        pass
+    #Прочитать одно письмо, вернуть в виде экземпляра класса Email, вспомогательный
+    def read_email(self, file_path):
+        subject = ""
+        text = []
+        is_main_text = False
+
+        with open(file_path, encoding="utf-8") as file:
+            lines = file.read().splitlines()
+
+        for line in lines:
+            line = line.lower()
+
+            # Добавляем строку в текст, если она не пустая
+            if is_main_text and line.strip()!= "":
+                text.append(line)
+                continue
+
+            # Если есть индикатор темы, считываем eё
+            if any(x for x in self.subject_v if x in line):
+                subject = line.split(":", 1)[1]
+                is_main_text = True
+
+        # переводим полученый текст в строку
+        text = "\n".join(text)
+        # Возвращаем
+        email = Email(file_path.name, file_path, subject, text)
+        return email
 
---
+++ src/email_class.py
@@ -0,0 +1,6 @@
+class Email:
+    def __init__(self,file_name, path, subject, text):
+        self.file_name = file_name
+        self.path = path
+        self.subject = subject
+        self.text = text
